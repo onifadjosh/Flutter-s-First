24c42752263c86f0dfb704ca5b444376f86e2fe1
Onifade Joshua <onifadjosh@gmail.com>
2024-04-28 23:34:09 +0100

    Update imports and navigator destinations to use snake\_case; change button style

---
+++ lib/sign_In.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/main.dart';
+import 'package:hello_world/reusable_button.dart';
+
+
+
+class Verify extends StatelessWidget {
+  const  Verify({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon:const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+      ),
+      ),
+
+      body: Padding(
+          padding:EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+        const Text('Sign In', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600,),),
+        const Text('Sign in to your Pairdel account.', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),),
+        const TextFieldMail(),
+        const SizedBox(height: 12),
+        const Password(),
+        const ForgetPassword(),
+        const ElevatedButtonSample(),
+       const  NoAccount(),
+        const Or(),
+        Padding(padding: EdgeInsets.all(0),
+        child: Column(
+          children: <Widget>[
+            
+            ReusableButton(text:'SignIn with Apple', colorr: Color(0xFF949494), onPressed: (){}, color:Color(0xFFF2F4F7), imageProvider:AssetImage('lib/assets/images/apple.png')),
+           const SizedBox(height: 12),
+            ReusableButton(text:'SignIn with Apple', colorr: Color(0xFF949494),onPressed: (){}, color:Color(0xFFF2F4F7), imageProvider:AssetImage('lib/assets/images/apple.png')),
+            ReusableButton(text:'SignIn with Apple', colorr: Color(0xFF949494),onPressed: (){}, color:Color(0xFFF2F4F7), imageProvider:AssetImage('lib/assets/images/apple.png')),
+          ],
+        ),
+        )
+      ],
+      ),
+      )
+    );
+  }
+}
+
+
+
+
+class TextFieldMail extends StatefulWidget {
+  const TextFieldMail({super.key});
+
+  @override
+  State<TextFieldMail> createState() => _TextFieldMailState();
+}
+
+class _TextFieldMailState extends State<TextFieldMail> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: double.infinity,
+      height: 56,
+      
+        child:TextField(
+        decoration: InputDecoration(
+          label: Text('Email Address'),
+          focusColor: Colors.green,
+          border: InputBorder.none,
+          labelStyle: TextStyle( color: Color(0xFF949494), fontSize: 16, fontWeight: FontWeight.w400,),
+          
+          fillColor: Color(0xFFF2F4F7),
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 14),
+          
+        ),
+        cursorColor: Colors.green,
+      ),
+     
+    );
+  }
+}
+
+class Password extends StatelessWidget {
+  const Password({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: double.infinity,
+      height: 56,
+        
+        child:TextField(
+          obscureText: true,
+        decoration: InputDecoration(
+          label: Text('Password'),
+          focusColor: Colors.green,
+          border: InputBorder.none,
+          labelStyle: TextStyle( color: Color(0xFF949494), fontSize: 16, fontWeight: FontWeight.w400,),
+          
+          fillColor: Color(0xFFF2F4F7),
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 14),
+          
+        ),
+          cursorColor: Colors.green,
+      ),
+     
+    );
+  }
+}
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // onTap: () {
+      //   Navigator.pushNamed(context, routeName);
+      // },
+
+      child:const Text('Forgot Password?',
+      style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 14,
+          color: Color(0xFF1D2939),
+          decoration: TextDecoration.underline,)
+          ), 
+    );
+  }
+}
+
+class NoAccount extends StatelessWidget {
+  const NoAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // onTap: () {
+      //   Navigator.pushNamed(context, routeName);
+      // },
+
+      child:const Text('No account? Create one',
+      style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 14,
+          color: Color(0xFF1D2939),
+          decoration: TextDecoration.underline,)
+          ), 
+    );
+  }
+}
